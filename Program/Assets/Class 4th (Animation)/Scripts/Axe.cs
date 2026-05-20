1b487491d97b1ee9a9f14fc6da07d699c055868c
Unity2033 <82032086+Unity2033@users.noreply.github.com>
2026-05-20 21:43:35 +0900

    (2026.5.20) 애니메이터 컨트롤러 시스템

--- Program/Assets/Class 4th (Animation)/Scripts/Axe.cs
+++ Program/Assets/Class 4th (Animation)/Scripts/Axe.cs
@@ -2,8 +2,30 @@ using UnityEngine;
 
 public class Axe : MonoBehaviour
 {
+    public int strikes;
+    public float proficiency;
+
     public void Attack(int count)
     {
-        
+        strikes += count;
+
+        if (strikes % 10 == 0)
+        {
+            Debug.Log("Critical...");
+        }
+    }
+
+    public void Throw(float experience)
+    {
+        if(proficiency < 100)
+        {
+            proficiency += experience;
+
+            Debug.Log("Proficiency : " + proficiency + "%");
+        }
+        else
+        {
+            Debug.Log("I have Mastered the skill");
+        }
     }
 }
