92f41d459299979b3adc9e3d585efb973c8d7626
Unity2033 <82032086+Unity2033@users.noreply.github.com>
2026-05-22 21:43:50 +0900

    (2026.5.22) 시간 시스템

--- Program/Assets/Class 5th (Animator Controller)/Scripts/AnimatorManager.cs
+++ Program/Assets/Class 5th (Animator Controller)/Scripts/AnimatorManager.cs
@@ -3,25 +3,65 @@ using UnityEngine;
 public class AnimatorManager : MonoBehaviour
 {
     public Animator animator;
+    public AnimatorStateInfo animatorStateInfo;
 
     public void Idle()
     {
-        animator.SetBool("Walk", false);
-        animator.SetBool("Attack", false);
+        animatorStateInfo = animator.GetCurrentAnimatorStateInfo(0);
+
+        if (animatorStateInfo.IsName("Walk"))
+        {
+            animator.SetBool("Idle", true);
+            animator.SetBool("Walk", false);
+        }
+        else if (animatorStateInfo.IsName("Attack"))
+        {
+            animator.SetBool("Idle", true);
+            animator.SetBool("Attack", false);
+        }
     }
 
     public void Walk()
     {
-        animator.SetBool("Walk", true);
+        animatorStateInfo = animator.GetCurrentAnimatorStateInfo(0);
+
+        if (animatorStateInfo.IsName("Attack"))
+        {
+            animator.SetBool("Walk", true);
+            animator.SetBool("Attack", false);
+        }
+        else if(animatorStateInfo.IsName("Idle"))
+        {
+            animator.SetBool("Walk", true);
+            animator.SetBool("Idle", false);
+        }
     }
 
     public void Attack()
     {
-        animator.SetBool("Attack", true);
+        animatorStateInfo = animator.GetCurrentAnimatorStateInfo(0);
+
+        if (animatorStateInfo.IsName("Idle"))
+        {
+            animator.SetBool("Idle", false);
+            animator.SetBool("Attack", true);
+        }
+        else if (animatorStateInfo.IsName("Walk"))
+        {
+            animator.SetBool("Walk", false);
+            animator.SetBool("Attack", true);
+        }
     }
 
     public void Die()
     {
+        animatorStateInfo = animator.GetCurrentAnimatorStateInfo(0);
+
+        if (animatorStateInfo.IsName("Die") || animator.IsInTransition(0))
+        {
+            return;
+        }
+
         animator.SetTrigger("Die");
     }
 }
